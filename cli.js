74b72ecdefaf45afca91a70cd1a09bab7021b006
cdeel <carson.deel@gmail.com>
2023-04-25 21:24:48 -0400

    commit1

---
+++ cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import moment from "moment-timezone";
+import minimist from "minimist";
+import fetch from "node-fetch";
+
+const args = minimist(process.argv.slice(2));
+const timezone = moment.tz.guess;
+
+
+if (args.h) {
+    console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -z            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit.`);
+    process.exit(0);
+  }
